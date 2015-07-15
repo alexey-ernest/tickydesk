423eb7e4b6311f57c9cd9aa625cbd0b044c713c6
Alexey Melnikov <alexey@Andreys-iMac-3.local>
2015-07-15 14:02:51 +0300

    QPX client

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 var request = require('request');
 var debug = require('debug')('tickydesk:scanner');
+var QPX = require('./lib/qpx');
 
-var api_token = process.env.AVIASALES_API_TOKEN;
+var api_key = process.env.QPX_API_KEY;
+var qpx = new QPX(api_key);
+
+var tripOptions = {
+  adults: 1,
+  originAirpot: 'MOW',
+  destinationAirport: 'KJA',
+  date: '2015-08-15',
+  dateBack: '2015-08-29'
+};
+qpx.find(tripOptions, function (err, result) {
+  if (err) {
+    throw err;
+  }
+
+  console.log(result);
+});
